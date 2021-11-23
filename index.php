72f888c51a27c8d8ac348837bcba02d0c9c552e5
RIANI PURWANINGSIH <89917809+rianipurwaningsih@users.noreply.github.com>
2021-11-23 09:29:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h2>Login</h2>
+<form action="ceklogin.php" method="post">
+	<label>Username</label>
+	<input type="text" name="userlogin"><br><br>
+	<label>Password</label>
+	<input type="password" name="userpassword"><br><br>
+	<input type="submit">
+</form>
+
+</body>
+</html>
